7b894d6a416e9eb0cb3804eff2dafb2a5044751c
sethcarrino <sethcarrino@gmail.com>
2023-01-05 14:45:49 -0500

    only show toolbar when element is selected

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -28,34 +28,39 @@ function ToolbarImpl() {
   });
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: "10px",
-        left: "10px",
-        display: "flex",
-        padding: "10px",
-        gap: "10px",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "lightgrey",
-        borderRadius: "8px"
-      }}
-    >
-      <input 
-        type="range" min="2" max="25" 
-        value={selectedElement?.numPoints || 5}
-        onChange={(e)=>{
-          const count = e.target.value;
-          updateElementPoints(selectedElementId, count)
-        }} 
-      />
-      <p style={{margin: '0'}}>{`Coords: ${cursorCoordinates.x}, ${cursorCoordinates.y}`}</p>
-      <div style={{ display: 'flex', flexDirection: 'column'}}>
-        {selectedElementId && <button style={buttonStyles} className="button-alt" onClick={() => toggleElementLock(selectedElementId)}>{selectedElement?.locked ? "Unlock" : "Lock"}</button>}
-      </div>
-    </div>
+    <>
+      {selectedElementId && 
+        <div
+          style={{
+            position: "fixed",
+            top: "10px",
+            left: "10px",
+            display: "flex",
+            padding: "10px",
+            gap: "10px",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "lightgrey",
+            borderRadius: "8px"
+          }}
+        >
+          <input 
+            type="range" min="2" max="25" 
+            value={selectedElement?.numPoints || 5}
+            onChange={(e)=>{
+              const count = e.target.value;
+              updateElementPoints(selectedElementId, count)
+            }} 
+          />
+          <p style={{margin: '0'}}>{`Coords: ${cursorCoordinates.x}, ${cursorCoordinates.y}`}</p>
+          <div style={{ display: 'flex', flexDirection: 'column'}}>
+            <button style={buttonStyles} className="button-alt" onClick={() => toggleElementLock(selectedElementId)}>{selectedElement?.locked ? "Unlock" : "Lock"}</button>
+          </div>
+        </div>
+      }
+    </>
+    
   );
 }
 
